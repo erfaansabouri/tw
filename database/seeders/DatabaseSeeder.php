4299edb1f89f22322ca80b14ab05ec18829e3f24
Erfan Sabouri <erfaansabouri@gmail.com>
2025-09-05 11:24:00 +0330

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,14 +9,14 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        //$this->call(AdminSeeder::class);
-        //$this->call(ExerciseTypeSeeder::class);
-        //$this->call(ApplicationFeatureSeeder::class);
-        //$this->call(AvatarSeeder::class);
-        //$this->call(GoalSeeder::class);
-        //$this->call(PlanSeeder::class);
-        //$this->call(DaySeeder::class);
-        //$this->call(InstructionSeeder::class);
-        //$this->call(ExerciseSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(ExerciseTypeSeeder::class);
+        $this->call(ApplicationFeatureSeeder::class);
+        $this->call(AvatarSeeder::class);
+        $this->call(GoalSeeder::class);
+        $this->call(PlanSeeder::class);
+        $this->call(DaySeeder::class);
+        $this->call(InstructionSeeder::class);
+        $this->call(ExerciseSeeder::class);
     }
 }
